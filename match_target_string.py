0ee015214f4e4c3f0c839621754596e9abb0dfb3
Asia Morgenstern <135760956+asia-morgenstern@users.noreply.github.com>
2023-12-01 11:48:16 -0500

    Add files via upload

---
+++ match_target_string.py
@@ -0,0 +1,157 @@
+""" match_target_string - genetic algorithm that matches to a target string
+
+Asia Morgenstern
+21 November 2023
+"""
+
+import random
+
+# define global constants
+
+POPULATION_SIZE = 100
+
+GENES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-=" \
+    "!@#$%^&*()_+,./;'[]\<>?:{}| "
+
+TARGET = "I love Geeks for Geeks!"
+
+class Individual(list):
+    def __init__(self, chromosome):
+        """ __ init__ - initializes an Individual
+                      - contains two properties:  chromosome and fitness score
+        """
+        
+        self.chromosome = chromosome
+        self.fitness = self.calc_fitness()
+    
+    def gen_chromosome():
+        """ gen_chromosome - generates a chromosome
+                           - returns a list of randomly chosen characters
+        """
+        
+        global GENES
+        global TARGET
+        
+        chromosome = []
+        for i in range(len(TARGET)):
+
+            gene = random.choice(GENES)
+            chromosome.append(gene)
+    
+        return chromosome
+    
+    def calc_fitness(self):
+        """ calc_fitness - calculates and returns the fitness score
+                         - determines fitness based on number of differing characters
+        """
+        
+        global TARGET
+    
+        fitness = 0
+        for s_gene, t_gene in zip(self.chromosome, TARGET):
+            if s_gene != t_gene:
+                fitness += 1
+    
+        return fitness
+    
+    def mate(self, parent2):
+        """ mate - returns a child chromosome by crossing 2 parents and adding mutations
+        
+        - parent2 - one of 2 parents 
+        """
+        
+        # chances of mutations or using genes from parent 1 or 2
+        
+        mutation_percent = 0.1
+        p2_percent = 1 - mutation_percent
+        p1_percent = p2_percent/2
+        
+        # create a child chromosome
+        
+        child_chromosome = []    
+        for p1_gene, p2_gene in zip(self.chromosome, parent2.chromosome):
+            r = random.random()
+            
+            # choose gene
+            
+            if r < p1_percent:
+                child_chromosome.append(p1_gene)
+            elif r < p2_percent:
+                child_chromosome.append(p2_gene)
+            else:
+                child_chromosome.append(self.mutated_gene())
+    
+        return Individual(child_chromosome)
+    
+    def mutated_gene(self):
+        """ mutated_gene - mutates a gene
+                         - returns a randomly selected gene
+        """
+        
+        global GENES
+    
+        gene = random.choice(GENES)
+        return gene
+
+def print_individual(indiv, gen):
+    """ print_individual - prints generation, Individual with lowest fitness, and fitness
+    """
+    
+    curr_str = "".join(indiv.chromosome)
+    curr_fitness = indiv.fitness
+    print(f"Generation {gen}  String:  {curr_str}  Fitness:  {curr_fitness}")
+
+def main():
+    global POPULATION_SIZE
+    
+    # initial population
+    
+    population = []
+    for _ in range(POPULATION_SIZE):
+        gnome = Individual.gen_chromosome()
+        population.append(Individual(gnome))
+    
+    # perform genetic algorithm until convergence
+    
+    curr_gen = 1                    # current generation
+    converge = False    
+    while not converge:
+        # sort population on fitness score in ascending order
+        
+        population = sorted(population, key=lambda x : x.fitness)
+        print_individual(population[0], curr_gen)
+        
+        # algorithm converges if lowest fitness is 0
+        
+        if population[0].fitness == 0:
+            converge = True
+            break
+        
+        # create new generation
+        
+        new_generation = []
+        
+        # add top 10% to new generation (elitism)
+        
+        elite_percent = 0.1
+        n_elite = int(elite_percent*POPULATION_SIZE)
+        new_generation.extend(population[:n_elite])
+        
+        # generate remaining children using top 50% of old generation
+        
+        top_percent = 0.5
+        top = int(top_percent*POPULATION_SIZE)
+        
+        for _ in range(POPULATION_SIZE - n_elite):
+            parent1 = random.choice(population[:top])
+            parent2 = random.choice(population[:top])
+            child = parent1.mate(parent2)
+            new_generation.append(child)
+        
+        # set current population to new generation and update curr_gen
+        
+        population = new_generation
+        curr_gen += 1
+
+if __name__ == "__main__": 
+    main()
---
+++ transcendental_eq.py
@@ -0,0 +1,237 @@
+""" transcendental_eq - genetic algorithm that solves the transcendental equation cosx = x
+
+Asia Morgenstern
+21 November 2023
+"""
+
+import random
+import math
+
+# define global constants
+
+POPULATION_SIZE = 100
+
+MAX_VAL = math.pi/4
+
+PRECISION = 10
+
+class Individual(object):
+    def __init__(self, chromosome):
+        """ __ init__ - initializes an Individual
+                      - contains two properties:  chromosome and fitness score
+        """
+        
+        self.chromosome = chromosome
+        self.fitness = self.calc_fitness()
+    
+    def gen_chromosome():
+        """ gen_chromosome - generates a chromosome
+                           - returns a list of bits based on randomly chosen floats
+        """
+        
+        global MAX_VAL
+        global PRECISION
+        
+        n = random.random()*MAX_VAL             # random float [0, MAX_VAL)
+        n = float_to_bin(n, PRECISION)          # convert float to binary string
+        
+        chromosome = list(n)                    # convert string to list
+    
+        return chromosome
+    
+    def calc_fitness(self):
+        """ calc_fitness - calculates and returns the fitness score
+                         - determines fitness based on calculating cosx - x
+        """
+        
+        x = "".join(self.chromosome)            # convert list to string
+        x = bin_to_float(x)                     # convert binary string to float
+        
+        fitness = abs(math.cos(x) - x)          # calculate fitness
+        
+        return fitness
+    
+    def mate(self, parent2):
+        """ mate - returns a child chromosome by crossing 2 parents and adding mutations
+        
+        - parent2 - one of 2 parents 
+        """
+        
+        # chances of mutations or using genes from parent 1 or 2
+        
+        mutation_percent = 0.1
+        p2_percent = 1 - mutation_percent
+        p1_percent = p2_percent/2
+        
+        # create a child chromosome
+        
+        child_chromosome = []    
+        for p1_gene, p2_gene in zip(self.chromosome, parent2.chromosome):
+            r = random.random()
+            
+            # choose gene
+            
+            if p1_gene == ".":                  # add decimal point
+                child_chromosome.append(p1_gene)
+                continue
+    
+            if r < p1_percent:
+                child_chromosome.append(p1_gene)
+            elif r < p2_percent:
+                child_chromosome.append(p2_gene)
+            else:
+                child_chromosome.append(self.mutated_gene())
+    
+        return Individual(child_chromosome)
+    
+    def mutated_gene(self):
+        """ mutated_gene - mutates a gene
+                         - returns either 0 or 1
+        """
+        
+        r = random.randint(0, 1)
+        
+        gene = str(r)
+        
+        return gene
+
+def print_individual(indiv, gen):
+    """ print_individual - prints generation, Individual with lowest fitness, float val, and fitness
+    """
+    
+    curr_str = "".join(indiv.chromosome)
+    curr_val = bin_to_float(curr_str)
+    curr_fit = indiv.fitness
+    print(f"Generation {gen}  String:  {curr_str}  Val:  {curr_val}  Fitness:  {curr_fit}")
+
+def float_to_bin(n, precision):
+    """ float_to_bin - converts ints and floats to binary strings
+    
+    n - int or float to convert to binary
+      - assumes valid input
+    precision - number of decimal places in binary representation
+    """
+    
+    # convert int to float
+    
+    if type(n) is not float:
+        n = float(n)
+    
+    # split float into whole number and decimal strings
+    
+    n = str(n)
+    whole, decimal = n.split(".")
+    
+    # convert whole number to binary
+    
+    whole = int(whole)                          # convert string to int
+    whole = str(bin(whole))                     # convert int to binary string
+    bin_rep = whole[2:]                         # strip off "0x"
+    
+    # convert decimal portion to binary
+    
+    decimal = "0." + decimal                    # convert decimal portion to decimal
+    decimal = float(decimal)                    # convert string to float
+    
+    bin_rep += "."
+    for i in range(precision):                  # calculate bin_rep to set precision
+        decimal = 2*decimal                     # double decimal
+        bin_rep += str(int(decimal))            # add int portion to bin_rep
+        
+        if decimal >= 1:                        # subtract if decimal >= 1
+            decimal -= 1
+
+    return bin_rep
+
+def bin_to_float(bin_rep):
+    """ bin_to_float - convert binary string to float
+    
+    bin_rep - string or list to convert to float
+            - assumes valid input
+    """
+    
+    # convert list to string
+    
+    if type(bin_rep) is list:
+        bin_rep = "".join(bin_rep)
+    
+    # split string into whole number and decimal strings
+    
+    whole, decimal = bin_rep.split(".")
+    
+    # calculate float equivalent from binary whole
+    
+    ft_num = 0.0
+    exponent = 0
+    for i in range(len(whole) - 1, -1, -1):     # iterate backwards through string
+        multiplier = int(whole[i])              # determine multiplier, 0 or 1
+        ft_num += multiplier*2**exponent        # multiply by 2^n, n = exponent
+        
+        exponent += 1
+    
+    # calculate float equivalent from binary decimal portion
+    
+    exponent = -1
+    for i in range(0, len(decimal), 1):         # iterate forwards through string
+        multiplier = int(decimal[i])            # determine multiplier, 0 or 1
+        ft_num += multiplier*2**exponent        # multiply by 2^n, n = exponent
+        
+        exponent -= 1
+    
+    return ft_num
+
+def main():
+    global POPULATION_SIZE
+    
+    # initial population
+    
+    population = []
+    for _ in range(POPULATION_SIZE):
+        gnome = Individual.gen_chromosome()
+        population.append(Individual(gnome))
+    
+    # perform genetic algorithm until convergence
+    
+    curr_gen = 1                                # current generation
+    epsilon = 1e-3
+    converge = False    
+    while not converge:
+        # sort population on fitness score in ascending order
+        
+        population = sorted(population, key=lambda x : x.fitness)
+        print_individual(population[0], curr_gen)
+        
+        # algorithm converges if lowest fitness is less than epsilon
+        
+        if population[0].fitness < epsilon:
+            converge = True
+            break
+        
+        # create new generation
+        
+        new_generation = []
+        
+        # add top 10% to new generation (elitism)
+        
+        elite_percent = 0.1
+        n_elite = int(elite_percent*POPULATION_SIZE)
+        new_generation.extend(population[:n_elite])
+        
+        # generate remaining children using top 50% of old generation
+        
+        top_percent = 0.5
+        top = int(top_percent*POPULATION_SIZE)
+        
+        for _ in range(POPULATION_SIZE - n_elite):
+            parent1 = random.choice(population[:top])
+            parent2 = random.choice(population[:top])
+            child = parent1.mate(parent2)
+            new_generation.append(child)
+        
+        # set current population to new generation and update curr_gen
+        
+        population = new_generation
+        curr_gen += 1
+
+if __name__ == "__main__": 
+    main()
